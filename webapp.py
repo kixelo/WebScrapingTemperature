44c15aa21b55efdc901abca2f686e984b4ad2cf3
kixelo <igorolexik@gmail.com>
2024-02-07 17:15:48 +0100

    enhanced version, data stored in database (sqlite3)

--- webapp.py
+++ webapp.py
@@ -5,6 +5,6 @@ import pandas
 df = pandas.read_csv("data.txt")
 
 figure = px.line(x=df["date"], y=df["temperature"],
-                 labels={"x":"Date", "y": "Temperature (C)"})
+                 labels={"x": "Date", "y": "Temperature (C)"})
 
 st.plotly_chart(figure)
