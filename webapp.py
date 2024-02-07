221125989c6c7ec75e1373f2f4e26653264a8eec
Igor O <94803838+kixelo@users.noreply.github.com>
2024-02-07 19:05:32 +0100

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import plotly.express as px
-import pandas
+import sqlite3
 
-df = pandas.read_csv("data.txt")
+#df = pandas.read_csv("data.txt")
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
+cursor.execute("SELECT date FROM temperature")
+date = cursor.fetchall()
+date = [item[0] for item in date]
 
-figure = px.line(x=df["date"], y=df["temperature"],
+cursor.execute("SELECT temperature FROM temperature")
+temperature = cursor.fetchall()
+temperature = [item[0] for item in temperature]
+
+
+
+figure = px.line(x=date, y=temperature,
                  labels={"x": "Date", "y": "Temperature (C)"})
 
 st.plotly_chart(figure)
